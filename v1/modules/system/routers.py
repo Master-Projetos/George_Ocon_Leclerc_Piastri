3fbfd5fa307f00210d57bbe6cc65f11e62d90a1c
Eduardo <eduardo.vchaves23@gmail.com>
2026-09-16 17:43:07 -0300

    feat: Stroll, just Stroll

--- v1/modules/system/routers.py
+++ v1/modules/system/routers.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Request
 from core.limiter import limiter
+from .services import where_stroll_finish
 
 router = APIRouter()
 
@@ -11,3 +12,18 @@ def read_root(request: Request):
         "version" : "0.2.1",
         "description": "API criada para bater no muro"
     }
+
+@router.get("/Stroll")
+@limiter.limit("1/year")
+def did_stroll_retire(request: Request):
+    position = where_stroll_finish()
+    
+    if position <= 3:
+        return {"message": "Stroll got a podium????"}
+    if position <= 11 :
+        return {"message": "Stroll got points, maybe everone crash"}
+    if position <= 15 :
+        return {"message": "Catfish"}
+    if position <= 22 :
+        return {"message": "Catfish"}
+    return {"Aposentou"}
